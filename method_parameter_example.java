646651adcee3b442f8c84b7804bd419ed6d0c8a9
Goktug Ulku <goktugulku94@gmail.com>
2023-04-05 21:31:50 +0300

    Java methods parameter

---
+++ method_parameter_example.java
@@ -0,0 +1,11 @@
+public class method_parameter_example {
+    
+    static void myMethod(String fname){
+        System.out.println(fname + " Clark");
+    }
+    public static void main (String [] args){
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
