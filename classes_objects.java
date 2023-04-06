14cb88b83bf197b81aebc4451919176fbbcc3d5a
Goktug Ulku <goktugulku94@gmail.com>
2023-04-06 18:41:29 +0300

    java classes objects

--- classes_objects.java
+++ classes_objects.java
@@ -1,10 +1,10 @@
-public class Main {
-    int x = 5;
-  
-    public static void main(String[] args) {
-      Main myObj1 = new Main();  // Object 1
-      Main myObj2 = new Main();  // Object 2
-      System.out.println(myObj1.x);
-      System.out.println(myObj2.x);
-    }
+public class classes_objects{
+  int x = 5;
+
+  public static void main(String[] args) {
+    classes_objects myObj1 = new classes_objects();  // Object 1
+    classes_objects myObj2 = new classes_objects();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
   }
+}
